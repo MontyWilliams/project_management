4d77195d3a7448b74f8f8dea7adef12165036a8f
Montywilliams <3650@holbertonschool.com>
2022-11-15 04:50:13 -0600

    add relational field to ProjectType: the data has a field in each project that relates to a client: much eacier then SQL

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -30,7 +30,7 @@ const ProjectType = new GraphQLObjectType({
         client: {
             type: ClientType,
             resolve(parent, args) {
-                return clients.find((client ))
+                return clients.find((client) => client.id === parent.id);
             }
         }
     })
