1e5b9e6e430e570334c92cb466050733cee48e9d
Montywilliams <3650@holbertonschool.com>
2022-11-15 04:45:47 -0600

    restructure for readability

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -27,23 +27,29 @@ const ProjectType = new GraphQLObjectType({
         name: { type: GraphQLString},
         description: { type: GraphQLString},
         status: { type: GraphQLString}
+        client: {
+            type: ClientType,
+            resolve(parent, args) {
+                return clients.find((client ))
+            }
+        }
     })
 })
 
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
+        projects: {
+            type: new GraphQLList(ProjectType),
+            resolve(parent, args) {
+                return projects
+            }
+        },
         project: {
             type: ProjectType,
             args: {id: {type: GraphQLID}},
             resolve(parent, args) {
                 return projects.find((project) => project.id === args.id);
-            }
-        },
-        projects: {
-            type: new GraphQLList(ProjectType),
-            resolve(parent, args) {
-                return projects
             }
         },
         clients: {
