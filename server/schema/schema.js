b75f93500ad6be5ff9b32b0d19b01bc420fe95a5
Montywilliams <3650@holbertonschool.com>
2022-11-15 04:42:09 -0600

    add ProjectType to GSchema and added queries 2 queries: to get all the projects and filter through as a list: to get individual projects and filter through them

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -8,6 +8,7 @@ const {
     GraphQLList
 } = require('graphql')
 
+//ClientType
 const ClientType = new GraphQLObjectType({
     name: 'Client',
     fields: () => ({
@@ -18,12 +19,39 @@ const ClientType = new GraphQLObjectType({
     })
 })
 
+//ProjectType
+const ProjectType = new GraphQLObjectType({
+    name: 'Project',
+    fields: () => ({
+        id: { type: GraphQLID},
+        name: { type: GraphQLString},
+        description: { type: GraphQLString},
+        status: { type: GraphQLString}
+    })
+})
+
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
+        project: {
+            type: ProjectType,
+            args: {id: {type: GraphQLID}},
+            resolve(parent, args) {
+                return projects.find((project) => project.id === args.id);
+            }
+        },
+        projects: {
+            type: new GraphQLList(ProjectType),
+            resolve(parent, args) {
+                return projects
+            }
+        },
         clients: {
             type: new GraphQLList(ClientType),
-        }
+            resolve(parent, args) {
+                return clients
+            }
+        },
         client: {
             type: ClientType,
             args: { id: {type: GraphQLID}},
