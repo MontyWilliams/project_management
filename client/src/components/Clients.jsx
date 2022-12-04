8f9f0405e37bb70f426b09ec17854f3841317ab5
Montywilliams <3650@holbertonschool.com>
2022-12-04 15:39:48 -0600

    clean up

--- client/src/components/Clients.jsx
+++ client/src/components/Clients.jsx
@@ -19,9 +19,7 @@ export default function Clients() {
 
   return (
     <>
-        {
-            !loading && !error && <h1>Clients</h1>
-        }
+        {!loading && !error && <h1>Clients</h1>}
     </>
   )
 }
