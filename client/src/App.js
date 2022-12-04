88a8b2ecced6ee4bcf4a17d4f395574bc15438b4
Montywilliams <3650@holbertonschool.com>
2022-12-04 14:56:19 -0600

    add header to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
-import graphqllogo from './components/assets/graphql_logo.png'
+import Header from './components/Header.jsx';
 
 function App() {
   return (
-    <div className="container">
-      <h1>Whatup tho</h1>
-    </div>
+    <>
+      < Header />
+      <div className="container">
+        <h1>Whatup tho</h1>
+      </div>
+    </>
   );
 }
 
