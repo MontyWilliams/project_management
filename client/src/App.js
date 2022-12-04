353f25be63021ee4aeb6941834100ff4deac441a
Montywilliams <3650@holbertonschool.com>
2022-12-04 14:50:50 -0600

    clean up react app default conf

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import graphqllogo from './components/assets/graphql_logo.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Whatup tho</h1>
     </div>
   );
 }
