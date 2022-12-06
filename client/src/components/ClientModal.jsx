97223887682487dbbc59e0f54a8d0d5770d8cc86
Montywilliams <3650@holbertonschool.com>
2022-12-06 14:08:29 -0600

    add ADdClient to modal

--- client/src/components/ClientModal.jsx
+++ client/src/components/ClientModal.jsx
@@ -1,15 +1,35 @@
 import { useState } from 'react';
 import { FaUser } from 'react-icons/fa';
 import { useMutation } from '@apollo/client';
+import { ADD_CLIENT } from '../mutations/clientMutations.js';
+import { GET_CLIENTS } from '../queries/clientQueries.js';
 
 function ClientModal() {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [phone, setPhone] = useState('');
 
+    const [addClient] = useMutation(ADD_CLIENT, {
+      variables: { name, email, phone },
+      update(cache, { data: {addClient}}) {
+        // Read from cache
+        const { clients } = cache.readQuery({query:
+        GET_CLIENTS});
+        // write to client
+        cache.writeQuery({
+          query: GET_CLIENTS,
+          data: { clients: [...clients, addClient] },
+        });
+      }
+    });
+
     const onSubmit = (e) => {
         e.preventDefault();
-        console.log(name, email, phone);
+        if(name === '' || email === '' || phone === '') {
+          return alert('YOu gotta fill out all the boxes');
+        }
+        addClient(name, email, phone);
+
     }
 
   return (
--- client/src/components/Clients.jsx
+++ client/src/components/Clients.jsx
@@ -1,5 +1,5 @@
-import { gql, useQuery } from '@apollo/client';
-import ClientRow from './ClientRow'
+import { useQuery } from '@apollo/client';
+import ClientRow from './ClientRow';
 import { GET_CLIENTS } from '../queries/clientQueries.js'
 import Spinner from './Spinner'
 
