6ccdbeea2b5da5f2b17534db8b7dce384ccb4e1c
Montywilliams <3650@holbertonschool.com>
2022-12-06 05:21:22 -0600

    correction syntax

--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -1,6 +1,11 @@
 import { FaTrash } from 'react-icons/fa'
+import { useMutation } from '@apollo/client'
+import { DELETE_CLIENT } from '../mutations/clientMutations.js'
 
 export default function ClientRow({client}) {
+  // The 2nd parameter is the argument
+  const [deleteClient] = useMutation(DELETE_CLIENT, { variables: {id: client.id}});
+
   return (
     <tr>
         <td>{client.name}</td>
@@ -8,8 +13,9 @@ export default function ClientRow({client}) {
         <td>{client.phone}</td>
         <td>
             <button className="btn btn-danger btn-sm">
-                <FaTrash />
-            </button>
+              onClick={deleteClient}
+              <FaTrash />
+            </button> 
         </td>
     </tr>
   )
