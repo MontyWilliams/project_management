c240564d19d6ed8fd6c7a339ce43643d206adc49
Montywilliams <3650@holbertonschool.com>
2022-12-06 10:18:32 -0600

    add icon to modal button

--- client/src/components/ClientModal.jsx
+++ client/src/components/ClientModal.jsx
@@ -5,15 +5,18 @@ import { useMutation } from '@apollo/client';
 function ClientModal() {
   return (
     <>
-        <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-  Launch demo modal
+        <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#ClientModal">
+            <div className="d-flex align-items-center">
+                <FaUser className="icon" />
+                <div>Add User</div>
+            </div>
 </button>
 
-<div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div className="modal fade" id="ClientModal" tabindex="-1" aria-labelledby="ClientModalLabel" aria-hidden="true">
   <div className="modal-dialog">
     <div className="modal-content">
       <div className="modal-header">
-        <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <h1 className="modal-title fs-5" id="ClientModalLabel">Modal title</h1>
         <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div className="modal-body">
