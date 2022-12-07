d9632b2a504a200bf71d99638f3f9bf783c5be0e
Montywilliams <3650@holbertonschool.com>
2022-12-07 10:39:59 -0600

    import data using appolo useQuery as state manager

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { Link, useParams } from 'react-router-dom';
+import Spinner from '../components/Spinner';
+import { useQuery } from '@apollo/client';
+import { GET_PROJECT } from '../queries/projectQueries';
 
 export default function Project() {
+    const { id } = useParams();
+    const { loading, error, data } = useQuery(GET_PROJECT, { variables: { id }});
+    if ( loading ) return <Spinner />;
+    if ( error ) return <p>Ion know Bro...</p>
   return (
-    <div>Project</div>
+    <>
+        {!loading && !error && (
+            <div className="mx-auto w-75 card p-5">
+                <Link to="/" className="btn btn-light btn-sm w-25 d-inline ms-auto">
+                    back
+                </Link>
+            </div>
+        )}
+    </>
   )
 }
