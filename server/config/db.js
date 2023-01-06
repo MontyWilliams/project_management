ae60088b7d241443ebf9d0e6ff94c50282b5adc0
Monty WIlliams <90469237+MontyWilliams@users.noreply.github.com>
2023-01-05 22:17:57 -0600

    Delete server directory

--- server/config/db.js
+++
@@ -1,9 +0,0 @@
-const
-    mongoose = require('mongoose');
-    const connectDB = async () => {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-
-        console.log(`MogoDB Connected Bru: ${conn.connection.host}`.red.underline.bold);
-    }
-
-module.exports = connectDB;
